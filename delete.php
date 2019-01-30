f6df5efd17100e7e9508fcaad4ddbec20a8fb1f2
内田佳世 <pauole.noah.hoku@gmail.com>
2019-01-30 10:15:18 +0800

    削除処理実装

--- delete.php
+++ delete.php
@@ -1,4 +1,16 @@
 <?php
+require('dbconnect.php');
+
+//削除する投稿IDを取得
+$feed_id = $_GET['feed_id'];
+
+//削除SQL
+//DELETE FROM テーブル WHERE 条件
+$sql = 'DELETE FROM `feeds` WHERE `id` = ?';
+$data = [$feed_id];
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);
+
 
 header("Location: timeline.php");
 exit();
--- timeline.php
+++ timeline.php
@@ -151,8 +151,12 @@ while (true) {
                             <span class="like-count">10</span>
                             <a href="#collapseComment" data-toggle="collapse" aria-expanded="false"><span>コメントする</span></a>
                             <span class="comment-count">コメント数：5</span>
+
+                            <?php if ($feed['user_id'] == $signin_user['id']): ?>
                             <a href="edit.php" class="btn btn-success btn-xs">編集</a>
-                            <a onclick="return confirm('ほんとに消すの？');" href="#" class="btn btn-danger btn-xs">削除</a>
+                            <a onclick="return confirm('ほんとに消すの？');" href="delete.php?feed_id= <?php echo $feed['id']; ?>" class="btn btn-danger btn-xs">削除</a>
+                            <?php endif;?>
+
                         </div>
                         <?php include('comment_view.php'); ?>
                     </div>
